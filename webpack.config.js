ba3b433d57b277b27e881a076b26c730f88894e9
Spencer Davies <snwmn24@gmail.com>
2016-06-13 22:16:37 -0600

    just updating before redoing computer

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,10 @@ module.exports = {
     },
     module: {
         loaders: [
+            /*
             { test: /\.css$/, loader: "style!css" },
+            */
+            { test: /\.css$/, loader: "style-loader!css-loader" },
             { test: /\.ts$/, loader: 'ts' },
             { test: /\.scss$/, loader: 'style!css!sass?sourceMap' }
         ]
